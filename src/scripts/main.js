07a082ac6a463efc587144f13d8b2e40bcda4c5f
GavrilyukYura <gavrilyuk220@gmail.com>
2022-10-21 23:22:50 +0300

    Page was created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const btnContainer = document.getElementById('List');
+
+const btns = btnContainer.getElementsByClassName('nav__item');
+
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener('click', function(event) {
+    event.preventDefault();
+
+    const current = document.getElementsByClassName('nav__item--is-active');
+
+    current[0].className = current[0].className
+      .replace(' nav__item--is-active', '');
+    this.className += ' nav__item--is-active';
+  });
+}
